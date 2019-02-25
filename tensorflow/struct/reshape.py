c8a7d1e56982887d1d2e27c6290bf0ee74c5b1e8
yexuehua <yxh1164691705@qq.com>
2019-02-25 20:51:47 +0800

    add

--- tensorflow/struct/reshape.py
+++ tensorflow/struct/reshape.py
@@ -1,4 +1,10 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"]="2"
 import tensorflow as tf
-
+a = tf.constant([
+                [[1,2],[3,4],[5,6]],
+                [[7,8],[9,10],[11,12]]
+                ],tf.float32)
+sess = tf.Session()
+b = tf.reshape(a,[3,1,-1])
+print(sess.run(b))
